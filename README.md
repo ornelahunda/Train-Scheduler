af45be4ed166248a7e2b71bc97d942651f1adad6
ornelahunda <ornelahunda@gmail.com>
2018-03-18 19:15:29 -0400

    calculating Arrival time and minutes away using moment.js

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Train-Scheduler
+This website is created to help user with the trains schedule. It requires from user to enter the train they want to have information about, its destination and its frequency and this program finds out when the next train is coming and how many minutes is it away.
+
+Steps used to complete this task:
+
+1. Create Firebase Project on console.firebase.google.com.
+2. Initialise Firebase in the JS file.
+3. Create two separate panels on Html file, one to get user input and one to display the information required.
+4. Save user input in variables, sent it to database and display it in the screen.
+5. Create a way to calculate the time and min away.
+
 
 * I did not use a CSS styling sheet for this project. Insead I used bootstrap and made just some little modifications to it with inline CSS.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,95 @@
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyCgzWoqOlrRg11uM0uqJXXA7IS8kI8vgPw",
+            authDomain: "train-schedule-16ac2.firebaseapp.com",
+            databaseURL: "https://train-schedule-16ac2.firebaseio.com",
+            projectId: "train-schedule-16ac2",
+            storageBucket: "train-schedule-16ac2.appspot.com",
+            messagingSenderId: "974441390295"
+        };
+        firebase.initializeApp(config);
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCgzWoqOlrRg11uM0uqJXXA7IS8kI8vgPw",
-    authDomain: "train-schedule-16ac2.firebaseapp.com",
-    databaseURL: "https://train-schedule-16ac2.firebaseio.com",
-    projectId: "train-schedule-16ac2",
-    storageBucket: "train-schedule-16ac2.appspot.com",
-    messagingSenderId: "974441390295"
-};
-firebase.initializeApp(config);
-
-// Create a variable to reference the database
-var database= firebase.database();
-var name="";
-var destination="";
-var time="";
-var frequency=0;
-
-// when we click the submit button 
-// this gets the user input and saves it in different variables
-$("#addButton").on("click", function(){
-name=$("#name").val().trim();
-destination=$("#destination").val().trim();
-time=$("#time").val().trim();
-frequency=$("#frequency").val().trim();
+        // Create a variable to reference the database
+        var database = firebase.database();
 
 
-database.ref().set({
-    name:name,
-    destination:destination,
-    time:time,
-    frequency:frequency
+        $(document).ready(function () {
+                event.preventDefault();
 
-});
-$("#displayName").html(name);
-return false;
-});
+                // when we click the submit button 
+                // this gets the user input and stores it
+                $("#addButton").on("click", function () {
+                    // Initial values 
+                    var name = $("#name").val().trim();
+                    var destination = $("#destination").val().trim();
+                    var time = moment($("#time").val().trim(), "HH:mm").subtract(10, "years").format("X");
+                    var frequency = $("#frequency").val().trim();
 
-database.ref().on("value",function(snapshot){
 
-});
+                    // Create local object to temporary hold the train data
+                    database.ref().push({
+                        name: name,
+                        destination: destination,
+                        time: time,
+                        frequency: frequency,
+                        dateAdded: firebase.database.ServerValue.TIMESTAMP
+                    });
+                    // Push the data to the database
+
+
+                    // Empty text areas
+                    $("#name").val("");
+                    $("#destination").val("");
+                    $("#time").val("");
+                    $("#frequency").val("");
+
+                    // Prevents refreshing
+                    return false;
+                });
+
+
+
+
+                database.ref().on("child_added", function (snapshot) {
+
+
+                    // Change the HTML to reflect
+
+                    var databaseName = (snapshot.val().name);
+
+                    var databaseDestination = (snapshot.val().destination);
+
+                    var databaseFrequency = (snapshot.val().frequency);
+
+                    var databaseTime = (snapshot.val().time);
+
+
+
+                    var diffTime = moment().diff(moment.unix(databaseTime), "minutes");
+                    var timeRemainder = moment().diff(moment.unix(databaseTime), "minutes") % databaseFrequency;
+                    var minutes = databaseFrequency - timeRemainder;
+                    var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A");
+
+
+
+                    $("#displayName").append("<br>" + databaseName);
+
+                    $("#displayDestination").append("<br>" + databaseDestination);
+                    $("#displayFrequency").append("<br>" + databaseFrequency);
+                    $("#displayArrival").append("<br>" + nextTrainArrival);
+                    $("#displayMin").append("<br>" + minutes);
+                    // + "</td><td>" + firebaseLine + "</td><td>"+ 
+                    // firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "
+                    // </td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+
+
+
+
+                });
+
+            },
+            function (errorObject) {
+
+                console.log("Errors handled: " + errorObject.code);
+
+            });
